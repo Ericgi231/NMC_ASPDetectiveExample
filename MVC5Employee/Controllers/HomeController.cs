67273000adec75913ab8a004ca0fd497cba10c2d
Eric Grant <ericgi231@gmail.com>
2019-01-29 17:12:10 -0500

    Add project files.

---
+++ MVC5Employee/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Employee.Models;
+
+namespace MVC5Employee.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmp(int empId, string empName, int empSal)
+        {
+            Employee e = new Employee()
+            {
+                Id = empId,
+                Name = empName,
+                Salary = empSal
+            };
+            return View("EmpConfirm", e);
+        }
+
+        public ActionResult AddEmp()
+        {
+            return View("NewEmpForm");
+        }
+
+        public ActionResult GrossPayCalculator()
+        {
+            return View();
+        }
+
+        public ActionResult MainOfficeEmployees()
+        {
+            return View();
+        }
+    }
+}
